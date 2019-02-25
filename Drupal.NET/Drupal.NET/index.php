1e5a276579acb4644a77287045457d9d6e6264ae
JanPavlovsky <jan@pavlovsky.net>
2019-02-25 19:06:52 +0100

    Drupal index and basic files

--- Drupal.NET/Drupal.NET/index.php
+++ Drupal.NET/Drupal.NET/index.php
@@ -1,3 +1,24 @@
 <?php
 
-echo '"Hello World!"';
+/**
+ * @file
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
+ */
+
+use Drupal\Core\DrupalKernel;
+use Symfony\Component\HttpFoundation\Request;
+
+echo "Drupal.NET index start";
+
+$autoloader = require_once 'autoload.php';
+
+$kernel = new DrupalKernel('prod', $autoload5er);
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
